2270baefc429148d859ea01d27736b05b54bdeaf
Rabbit937 <hackrabbit@qq.com>
2024-04-29 14:26:03 +0800

    feat: update WTF-solidity

---
+++ WTF-Solidity/InsertionSort/InsertionSort.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract InsertionSort {
+    // if-else
+    function ifElseTest(uint256 _number) public pure returns (bool) {
+        if (_number == 0) {
+            return (true);
+        } else {
+            return (false);
+        }
+    }
+
+    // for loop
+    
+}
